e26921036879d6b6037750beaa9c544d63fa5cb3
y0ngyaw <yongyawyeo98@gmail.com>
2018-09-21 19:06:15 +0800

    Update chess logic

--- Position.java
+++ Position.java
@@ -1,4 +1,6 @@
-public class Position{
+import java.io.*;
+
+public class Position implements Serializable{
     public int x;
     public int y;
     Position(int x, int y){
@@ -14,5 +16,10 @@ public class Position{
     @Override
     public int hashCode(){
         return (this.x * 10) + this.y;
+    }
+
+    @Override
+    public String toString() {
+        return "X: " + this.x + " Y: " + this.y;
     }
 }
